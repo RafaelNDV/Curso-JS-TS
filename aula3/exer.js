254e10f3a37ea6f432ba68d40c5247eaffb7947f
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-20 13:26:25 -0300

    aula 3 feita

---
+++ aula3/exer.js
@@ -0,0 +1,2 @@
+console.log(`Meu nome é "Rafael Nestor". Estou aprendendo JavaScript às`, 13,`da tarde.`)
+console.log('Este', 'É', 'Um', 1000)
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Hello "World!"'); // String
+console.log("Rafael 'Nestor'");
+console.log(`'Luiz' "Otávio"`);
+
+console.log(123456, 15.86542323, 'Rafael Nestor'); // Number 
