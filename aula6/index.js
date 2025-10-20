ffeb049c6374d4f6fae2d07c5bc218ffa24c0c08
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-20 13:58:05 -0300

    aula6 feita

---
+++ aula6/index.js
@@ -0,0 +1,26 @@
+/*
+let nome; // Declarou a variável
+nome = "João"; // Inicializando a variável
+console.log(nome)
+nome = 'Otávio';
+console.log(nome)
+
+
+
+let nome = 'Rafael';
+
+console.log( nome ,'nasceu em 1984.');
+console.log('Em 2000',nome,'conheceu Maria.');
+console.log(nome,'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com',nome,'em 2015.');
+console.log('O filho de',nome,'se chama Eduardo.');
+*/
+
+// Não podemos criar variáveis com palavras reservadas. (let let)
+// Variáveis precisam ter nome significativos
+// Não podemos começar o nome de uma variável com um número (let 1nome)
+// Não podem conter espaços ou traços (let nome cliente ou nome-cliente)
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO ULTILIZE VAR, ULTILIZE LET
