ca93487e80840678499563a52f31b562e7a16d23
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-20 13:31:42 -0300

    aula4 feita

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -1,1 +1,11 @@
 // E escrever um comentário
+console.log('Olá Mundo!'); // Aqui tem outro comentário
+console.log('Linha 1'); // Algo aí cara
+console.log('Linha 2'); // testando comentários só
+/* Tem como comentar em bloco
+olha só que maneiro o comentário em bloco
+massa demais né */
+// console.log('Linha 3');
+// vem aí um comentario ein
+console.log('Linha 4');
+// Vai ser ignorado
