c6ff19f5fc713bb7a674f898af14f0fba4035a5b
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-20 23:04:46 -0300

    aula11 feita

---
+++ secao_2/aula11/js/script.js
@@ -0,0 +1,4 @@
+let num1 = Number(window.prompt('Digite um número: '))
+let num2 = Number(window.prompt('Digite outro número: '))
+const resultado = num1 + num2
+alert(`O resultado da soma dos números ${num1} e ${num2} é igual á ${resultado}`)
