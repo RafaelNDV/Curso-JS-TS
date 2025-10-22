624edb0c5aef1e201c3fff4690f28337fee8e5e0
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-22 15:50:01 -0300

    aula29 feita

---
+++ secao_3/aula29/index.js
@@ -0,0 +1,35 @@
+function getWeekDay(diaSemana){
+    let diaSemanaTexto;
+    
+    switch(diaSemana){
+    case 0: 
+        diaSemanaTexto = 'Domingo' 
+        break
+    case 1: 
+        diaSemanaTexto = 'Segunda' 
+        break
+    case 2: 
+        diaSemanaTexto = 'Terça' 
+        break
+    case 3: 
+        diaSemanaTexto = 'Quarta' 
+        break
+    case 4: 
+        diaSemanaTexto = 'Quinta' 
+        break
+    case 5: 
+        diaSemanaTexto = 'Sexta' 
+        break
+    case 6: 
+        diaSemanaTexto = 'Sábado' 
+        break
+    default:
+        diaSemanaTexto = ''
+        break
+    }
+    return diaSemanaTexto
+}
+
+const data = new Date()
+const diaSemana = data.getDay()
+console.log(getWeekDay(diaSemana))
