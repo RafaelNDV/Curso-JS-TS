7edfb71bb15e8ddeb12524094ea4eb840970efd6
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-22 18:23:32 -0300

    aula34 feita

---
+++ secao_3/aula34/index.js
@@ -0,0 +1,24 @@
+// console.log('Linha 1')
+// console.log('Linha 2')
+// console.log('Linha 3')
+// console.log('Linha 4')
+// console.log('Linha 5')
+
+// i - index
+// for ( let i = 0; i <= 5; i++){
+//     console.log(`Linha ${i}`)
+// }
+
+// for(let i = 1000; i >= 0; i -= 50){
+//     console.log(`Linha ${i}`)
+// }
+
+// for (let i = 0; i <= 20; i += 2){
+//     console.log(i)
+// }
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Otávio', 'Luiz', 'Rafael', 'João', 'Miranda']
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
