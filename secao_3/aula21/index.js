9b4ed57b6d739c8a3ac8a52ca6da89baa1a6b07c
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-22 09:50:26 -0300

    aula21 feita

---
+++ secao_3/aula21/index.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores de comparação
+ * > Maior que
+ * >= Maior que ou igual a
+ * < Menor que
+ * <= Menor que ou igual a
+ * == Igualdade (valor) [não é recomendado]
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor) [não é recomendado]
+ * !== diferente estrito (valor e tipo)
+ */
+
+const num1 = 10
+const num2 = 10
+const comp = num1 !== num2
+console.log(comp)
