23d27aa46110ae968512d78f61eada68a1cbf899
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-23 19:20:46 -0300

    aula41 feita

---
+++ secao3/aula41/index.js
@@ -0,0 +1,15 @@
+// Escreva uma função que recebe 2 números e retorne o mair deles
+
+// function maior(n1, n2){
+//     if(n1 === n2 || n1 > n2){
+//         return n1
+//     }else if(n2 > n1){
+//         return n2
+//     }else{
+//         return `Vish fi deu algum erro tu foi querer escrever ${n1} e ${n2}`
+//     }
+// }
+
+const max = (n1, n2) => x > y ? n1 : n2 // forma de fazer com condição ternária e arrow function
+
+console.log(maior(15, 'a'))
