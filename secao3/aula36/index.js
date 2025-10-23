ca5770db6be78444d268f3c84428d6af7d78983b
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-23 18:18:24 -0300

    aula36 feita

--- secao3/aula36/index.js
+++ secao3/aula36/index.js
@@ -1,5 +1,23 @@
-const frutas = ['Pera', 'Maçã', 'Uva']
-
-for (let i = 0; i < frutas.length; i++){
-    console.log()
+// for in lê chacves de um obejto também
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+    idade: 30
 }
+
+for (let i in pessoa){
+    console.log(i) // retorna as chaves
+    console.log(pessoa[i])  // retorna os valores que estão nas chaves
+}
+
+
+// const frutas = ['Pera', 'Maçã', 'Uva']
+
+// for (let i = 0; i < frutas.length; i++){
+//     console.log(frutas[i])
+// }
+
+// // Posso fazer for in em arrays, e ele me retorna o índice
+// for (let i in frutas){
+//     console.log(frutas[i])
+// }
