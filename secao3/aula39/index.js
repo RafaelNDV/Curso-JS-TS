ea03185bc0742bf7e83323092ef9ec13a62d9c8a
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-23 19:01:53 -0300

    aula39 feita

--- secao3/aula39/index.js
+++ secao3/aula39/index.js
@@ -1,4 +1,29 @@
-let i = 0
-while (i <= 10){
-    console.log(i)
+function random(min, max){
+    const r = Math.random() * (max - min) + min
+    return Math.floor(r)
 }
+
+let rand = random(1, 50);
+console.log(rand)
+
+// Checa se é verdadeiro depois faz
+while (rand !== 10){
+    rand = random(1, 50)
+    console.log(rand)
+}
+
+// Faz depois checa se é verdadeiro
+do{
+    rand = random(1, 50)
+    console.log(rand)
+}while(rand !== 10)
+
+
+
+// USADO COMO FOR
+// const nome = 'Rafael'
+// let i = 0
+// while (i < nome.length){
+//     console.log(nome[i])
+//     i++
+// }
