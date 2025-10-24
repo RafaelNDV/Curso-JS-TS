616d02253f7238e1784e9d5af653c040187fd1f5
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-24 19:05:52 -0300

    aula48 meio feita
    fiz sozinho e ainda não deixa salvo, e minha resolução foi bizarra

---
+++ secao3/aula48/assets/js/main.js
@@ -0,0 +1,47 @@
+const enviar = document.querySelector('.enviar')
+const lista = document.querySelector('.lista')
+let btnApaga;
+let i = 0
+
+function adicionarLista(tarefa){
+    let li = document.createElement('li')
+    li.classList.add('li' + i)
+    lista.appendChild(li)
+    
+    let linha = document.createElement('p')
+    linha.classList.add('linha' + i)
+    li.appendChild(linha)
+
+
+    let criado = document.createElement('strong')
+    criado.classList.add('tarefa' + i)
+    criado.innerHTML = `${tarefa} - `
+    
+    let botao = document.createElement('button')
+    botao.innerHTML = 'Apagar'
+    botao.classList.add('apaga')
+    botao.id = 'apaga' + i
+
+    linha.appendChild(criado)
+    linha.appendChild(botao)
+
+    i++
+}
+
+function apagarTarefa(){
+
+}
+
+enviar.addEventListener('click', function(e){
+    let tarefa = document.querySelector('.tarefa').value
+    adicionarLista(tarefa)
+})
+
+document.addEventListener('click', function(e){
+    const el = e.target
+    if(el && el.classList.contains('apaga')){
+            const indice = el.id.replace('apaga', '')
+            const apagado = document.querySelector('.li' + indice)
+            apagado.remove()
+        }
+})
