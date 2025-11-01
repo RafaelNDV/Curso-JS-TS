f4f73dd7fd509aa9838092be3d2b1351bfbabf16
Rafael Nestor <rafaelnestor01@gmail.com>
2025-11-01 14:57:05 -0300

    aula90 feita
    fetch API

--- secao8/aula89/assets/js/main.js
+++ secao8/aula89/assets/js/main.js
@@ -1,16 +1,16 @@
-const request = obj => {
-    const xhr = new XMLHttpRequest()
-    xhr.open(obj.method, obj.url, true)
-    xhr.send(null)
+// const request = obj => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open(obj.method, obj.url, true)
+//     xhr.send(null)
 
-    xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300){
-            obj.success(xhr.responseText)
-        }else{
-            obj.error(xhr.statusText)
-        }
-    })
-}
+//     xhr.addEventListener('load', () => {
+//         if(xhr.status >= 200 && xhr.status < 300){
+//             obj.success(xhr.responseText)
+//         }else{
+//             obj.error(xhr.statusText)
+//         }
+//     })
+// }
 
 document.addEventListener('click', e => {
     const el = e.target
@@ -22,19 +22,18 @@ document.addEventListener('click', e => {
     }
 })
 
-function carregaPagina(el){
-    const href = el.getAttribute('href')
-    
-    request({
-        method: 'GET',
-        url: href,
-        success(response){
-            carregaResultado(response)
-        },
-        error(errorText){
-            console.log(errorText)
-        }
-    })
+async function carregaPagina(el){
+    try{
+        const href = el.getAttribute('href')
+        const response = await fetch(href)
+
+        if(response.status !== 200) throw new Error('ERRO 404!')
+
+        const html = await response.text()
+        carregaResultado(html)
+    } catch(e){
+        console.log(e)
+    }
 }
 
 function carregaResultado(response){
