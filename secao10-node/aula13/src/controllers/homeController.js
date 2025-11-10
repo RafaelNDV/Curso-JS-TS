b408e5cf0ba08ca7e235a634fe5f6b177f79b68f
Rafael Nestor <rafaelnestor01@gmail.com>
2025-11-10 16:56:46 -0300

    aula13 node feita
    middlewares

---
+++ secao10-node/aula13/src/controllers/homeController.js
@@ -0,0 +1,9 @@
+exports.paginaInicial = (req, res) => {
+    console.log('Respondendo o cliente')
+    res.render('index')
+    return
+}
+
+exports.trataPost = (req, res) => {
+    res.send('Ei sou sua nova rota de POST')
+}
