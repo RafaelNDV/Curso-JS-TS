3e720989e69c24a023e808ffbe8cfe25086e8961
Rafael Nestor <rafaelnestor01@gmail.com>
2025-11-13 14:07:08 -0300

    continuando projeto
    editando e apagando contatos

--- projetoagenda/src/models/ContatoModel.js
+++ projetoagenda/src/models/ContatoModel.js
@@ -16,10 +16,20 @@ function Contato(body){
     this.contato = null
 }
 
+// Métodos estáticos
 Contato.buscaPorId = async function (id){
     if(typeof id !== 'string') return
-    const user = await ContatoModel.findById(id.trim())
-    return user
+    const contato = await ContatoModel.findById(id.trim())
+    return contato
+}
+Contato.buscaContatos = async function (){
+    const contatos = await ContatoModel.find().sort({criadoEm: -1})
+    return contatos
+}
+Contato.delete = async function (id){
+    if(typeof id !== 'string') return
+    const contato = await ContatoModel.findOneAndDelete(id)
+    return contato
 }
 
 Contato.prototype.register = async function(){
