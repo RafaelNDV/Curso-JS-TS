14570ca26bb0e5e648238970b93ac441fdfe1a75
RafaelNDV <rafaelnestor01@gmail.com>
2025-11-23 12:36:06 -0300

    criando seeds

---
+++ protjetoAPI/src/database/seeds/20251123152720-criar-usuarios.js
@@ -0,0 +1,40 @@
+const bcryptjs = require('bcryptjs')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.bulkInsert('users', [
+    {
+      nome: 'Luiz',
+      email: 'luiz1@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      nome: 'Luiz 2',
+      email: 'luiz2@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      nome: 'Luiz 3',
+      email: 'luiz3@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      nome: 'Luiz 4',
+      email: 'luiz4@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+    ], {});
+
+  },
+
+  async down () {}
+};
--- protjetoAPI/src/routes/UserRoutes.js
+++ protjetoAPI/src/routes/UserRoutes.js
@@ -5,7 +5,7 @@ import loginRequired from '../middlewares/loginRequired.js'
 
 const router = new Router()
 
-// router.get('/', userController.index)
+router.get('/', userController.index)
 // router.get('/:id', userController.show)
 
 
