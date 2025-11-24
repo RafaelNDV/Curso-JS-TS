2845ae4c89f6b97e05f36b82a8df3d64b54d2432
RafaelNDV <rafaelnestor01@gmail.com>
2025-11-24 12:10:44 -0300

    recebendo fotos atraves do multer

--- protjetoAPI/app.js
+++ protjetoAPI/app.js
@@ -4,10 +4,12 @@ dotenv.config()
 import './src/database/index.js'
 
 import express from 'express'
+
 import homeRoutes from './src/routes/homeRoutes.js'
 import userRoutes from './src/routes/UserRoutes.js'
 import tokenRoutes from './src/routes/tokenRoutes.js'
 import alunoRoutes from './src/routes/alunoRoutes.js'
+import fotoRoutes from './src/routes/fotoRoutes.js'
 
 class App{
   constructor(){
@@ -26,6 +28,7 @@ class App{
     this.app.use('/users/', userRoutes)
     this.app.use('/tokens/', tokenRoutes)
     this.app.use('/alunos/', alunoRoutes)
+    this.app.use('/fotos/', fotoRoutes)
   }
 }
 
